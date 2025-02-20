31f06fe87dea0a4350f75216cc9e52be3d538abd
Lipson07 <opka0180@gmail.com>
2025-02-20 22:45:15 +0300

    fetch update

--- backend/app/app/static/js/admin/fetch/reg_fetch.js
+++ backend/app/app/static/js/admin/fetch/reg_fetch.js
@@ -2,60 +2,155 @@
 async function  regaccept() {
   
    
-    const res = await fetch("http://localhost:8080/admin/database_schema");
-    data =await res.json();
-    console.log(data);
-    let a=Object.keys(data)
-    for(let i=0;i<a.length;i++){
-       
-        let namedb=a[i];
-        let db=data[namedb].length;
-        let bd_size="-";
-        let active="-";
-        let table=document.querySelector(".table");
-        let sidebar=document.querySelector(".sidebar");
-        if(i<5){
-        table.innerHTML+="<tr><td>"+namedb+"</td><td>"+db+"</td><td>"+bd_size+"</td><td>"+active+"<td><button class='btn btn-success'>Изменить</button><button class='btn btn-danger'>Удалить</button></td>"+"</tr>";
-        }
-       waitForButtonClick('.page-btn1').then(() => {
-        s=5;
-        
-        let namedb1=a[i+s];
-        console.log(i);
-        let db1=data[namedb].length;
-        if(i<5){
-            table.innerHTML+="<tr><td>"+namedb1+"</td><td>"+db1+"</td><td>"+bd_size+"</td><td>"+active+"<td><button class='btn btn-success'>Изменить</button><button class='btn btn-danger'>Удалить</button></td>"+"</tr>";
-        }
-       
-       })
-        sidebar.innerHTML+="<a href='#' class='nav-item'>"+namedb+"</a>";
-        console.log(namedb,db,bd_size,active);
-   }
- 
+  const res = await fetch("http://localhost:8080/admin/database_schema");
+  data =await res.json();
+  console.log(data);
+  let a=Object.keys(data)
   
+  for(let i=0;i<a.length;i++){
      
-  
+      let namedb=a[i];
+      let db=data[namedb].length;
+      let bd_size="-";
+      let active="-";
+      let table=document.querySelector(".table");
+      let sidebar=document.querySelector(".sidebar");
     
-    
-}
-async function waitForButtonClick(buttonSelector) {
-    return new Promise(resolve => {
-      const button = document.querySelector(buttonSelector);
-  
-      if (!button) {
-        throw new Error(`Button with selector "${buttonSelector}" not found.`);
+      
+      
+   if(i<5){
+      table.innerHTML+="<tr><td>"+namedb+"</td><td>"+db+"</td><td>"+bd_size+"</td><td>"+active+"<td><button class='btn btn-success'>Изменить</button><button class='btn btn-danger'>Удалить</button></td>"+"</tr>";
       }
       
-      button.addEventListener('click', () => {
-        document.querySelector(".table").innerHTML = "";
-        setTimeout(() => {
-            resolve();
-        }, 100);
+       sidebar.innerHTML+="<a href='#' class='nav-item1'>"+namedb+"</a>";
         
-      }, { once: true }); 
-    });
-  }
+      
+    console.log(namedb,db,bd_size,active);
+
+
+   
+
   
-document.addEventListener("DOMContentLoaded", function() {
-    regaccept();
-  });
+  
+}
+
+}
+async function page1() {
+let table=document.querySelector(".table");
+table.innerHTML="";
+const res = await fetch("http://localhost:8080/admin/database_schema");
+data =await res.json();
+console.log(data);
+let a=Object.keys(data)
+table.innerHTML+="<thead><tr><th>Название таблицы</th><th>Количество записей</th><th>Размер</th><th>Последнее обновление</th><th>Действия</th></tr></thead>"
+for(let i=5;i<a.length;i++){
+  
+  let namedb=a[i];
+  let db=data[namedb].length;
+  let bd_size="-";
+  let active="-";
+  let table=document.querySelector(".table");
+
+  if(i>5&&i<11){
+  table.innerHTML+="<tr><td>"+namedb+"</td><td>"+db+"</td><td>"+bd_size+"</td><td>"+active+"<td><button class='btn btn-success'>Изменить</button><button class='btn btn-danger'>Удалить</button></td>"+"</tr>";
+  }
+} }
+async function page2() {
+let table=document.querySelector(".table");
+table.innerHTML="";
+const res = await fetch("http://localhost:8080/admin/database_schema");
+data =await res.json();
+console.log(data);
+let a=Object.keys(data)
+table.innerHTML+="<thead><tr><th>Название таблицы</th><th>Количество записей</th><th>Размер</th><th>Последнее обновление</th><th>Действия</th></tr></thead>"
+for(let i=10;i<a.length;i++){
+  
+  let namedb=a[i];
+  let db=data[namedb].length;
+  let bd_size="-";
+  let active="-";
+  let table=document.querySelector(".table");
+
+  if(i>10&&i<16){
+  table.innerHTML+="<tr><td>"+namedb+"</td><td>"+db+"</td><td>"+bd_size+"</td><td>"+active+"<td><button class='btn btn-success'>Изменить</button><button class='btn btn-danger'>Удалить</button></td>"+"</tr>";
+  }
+} }
+async function page3() {
+let table=document.querySelector(".table");
+table.innerHTML="";
+const res = await fetch("http://localhost:8080/admin/database_schema");
+data =await res.json();
+console.log(data);
+let a=Object.keys(data)
+table.innerHTML+="<thead><tr><th>Название таблицы</th><th>Количество записей</th><th>Размер</th><th>Последнее обновление</th><th>Действия</th></tr></thead>"
+for(let i=15;i<a.length;i++){
+  
+  let namedb=a[i];
+  let db=data[namedb].length;
+  let bd_size="-";
+  let active="-";
+  let table=document.querySelector(".table");
+
+  if(i>15&&i<21){
+  table.innerHTML+="<tr><td>"+namedb+"</td><td>"+db+"</td><td>"+bd_size+"</td><td>"+active+"<td><button class='btn btn-success'>Изменить</button><button class='btn btn-danger'>Удалить</button></td>"+"</tr>";
+  }
+} }
+async function page4() {
+let table=document.querySelector(".table");
+table.innerHTML="";
+const res = await fetch("http://localhost:8080/admin/database_schema");
+data =await res.json();
+console.log(data);
+let a=Object.keys(data)
+table.innerHTML+="<thead><tr><th>Название таблицы</th><th>Количество записей</th><th>Размер</th><th>Последнее обновление</th><th>Действия</th></tr></thead>"
+for(let i=20;i<a.length;i++){
+  
+  let namedb=a[i];
+  let db=data[namedb].length;
+  let bd_size="-";
+  let active="-";
+  let table=document.querySelector(".table");
+
+  if(i>20&&i<26){
+  table.innerHTML+="<tr><td>"+namedb+"</td><td>"+db+"</td><td>"+bd_size+"</td><td>"+active+"<td><button class='btn btn-success'>Изменить</button><button class='btn btn-danger'>Удалить</button></td>"+"</tr>";
+  }
+} }
+async function page() {
+  let table=document.querySelector(".table");
+  table.innerHTML="";
+  const res = await fetch("http://localhost:8080/admin/database_schema");
+  data =await res.json();
+  console.log(data);
+  let a=Object.keys(data)
+  table.innerHTML+="<thead><tr><th>Название таблицы</th><th>Количество записей</th><th>Размер</th><th>Последнее обновление</th><th>Действия</th></tr></thead>"
+  for(let i=0;i<a.length;i++){
+    
+    let namedb=a[i];
+    let db=data[namedb].length;
+    let bd_size="-";
+    let active="-";
+    let table=document.querySelector(".table");
+  
+    if(i<5){
+    table.innerHTML+="<tr><td>"+namedb+"</td><td>"+db+"</td><td>"+bd_size+"</td><td>"+active+"<td><button class='btn btn-success'>Изменить</button><button class='btn btn-danger'>Удалить</button></td>"+"</tr>";
+    }
+  } }
+  
+
+regaccept().then( a=>{
+  const bd=document.querySelectorAll(".nav-item1");
+  console.log(bd);
+        bd.forEach(bd=>{
+       
+          bd.addEventListener("click",function(){
+            window.location.href = '/admin_db_tables';
+            localStorage.setItem("db",bd)
+          })});
+  
+        } )
+  
+  document.querySelector(".page-btn0").addEventListener("click", page)
+  document.querySelector(".page-btn1").addEventListener("click", page1)
+  document.querySelector(".page-btn2").addEventListener("click", page2)
+  document.querySelector(".page-btn3").addEventListener("click", page3)
+  document.querySelector(".page-btn4").addEventListener("click", page4)
