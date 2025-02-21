abda3c6c83e529c34ac99b5981b56a94f9bfeae2
Lipson07 <opka0180@gmail.com>
2025-02-22 00:35:24 +0300

    fetch update 2.1

--- backend/app/app/static/js/admin/fetch/admin_db_table_fetch.js
+++ backend/app/app/static/js/admin/fetch/admin_db_table_fetch.js
@@ -3,15 +3,15 @@ const storedData = localStorage.getItem('db');
 console.log(storedData);
 let s=storedData.split('');
 s.pop()
-let ss=s.join('');
-console.log(data);
+let name=s.join('');
+console.log(name);
 if (storedData) {
     document.querySelector(".namebd").textContent = "Записи таблицы " + storedData;
 
 }
 async function regaccept() {
 
-    const res = await fetch(`http://www.intelligent-digital-systems.ru/${data}/read`);
+    const res = await fetch(`http://www.intelligent-digital-systems.ru/${name}/read`);
     data = await res.json();
    
     let a = Object.keys(data)
