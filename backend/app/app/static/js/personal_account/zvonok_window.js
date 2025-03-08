8bdbd8f483779615be82ec41f41719fdd7922438
Lipson07 <opka0180@gmail.com>
2025-03-08 22:34:32 +0300

    zvonki

---
+++ backend/app/app/static/js/personal_account/zvonok_window.js
@@ -0,0 +1,66 @@
+let isDraggingPopup1 = false;
+let isDraggingPopup2 = false;
+let offsetX1, offsetY1, offsetX2, offsetY2;
+
+let popup1 = document.querySelector(".vizov-window");
+let popup2 = document.querySelector(".video-window");
+
+document.querySelector(".vizov").addEventListener("click", () => {
+  popup1.style.display = "flex";
+});
+
+document.querySelector("#closeButton").addEventListener("click", () => {
+  popup1.style.display = "none";
+});
+
+popup1.addEventListener("mousedown", (e) => {
+  isDraggingPopup1 = true;
+  offsetX1 = e.clientX - popup1.offsetLeft;
+  offsetY1 = e.clientY - popup1.offsetTop;
+  popup1.style.cursor = "grabbing";
+});
+
+document.addEventListener("mouseup", () => {
+  isDraggingPopup1 = false;
+  popup1.style.cursor = "grab";
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (!isDraggingPopup1) return;
+
+  const x = e.clientX - offsetX1;
+  const y = e.clientY - offsetY1;
+
+  popup1.style.left = x + "px";
+  popup1.style.top = y + "px";
+});
+
+document.querySelector(".videos").addEventListener("click", () => {
+  popup2.style.display = "flex";
+});
+
+document.querySelector("#closeButton1").addEventListener("click", () => {
+  popup2.style.display = "none";
+});
+
+popup2.addEventListener("mousedown", (e) => {
+  isDraggingPopup2 = true;
+  offsetX2 = e.clientX - popup2.offsetLeft;
+  offsetY2 = e.clientY - popup2.offsetTop;
+  popup2.style.cursor = "grabbing";
+});
+
+document.addEventListener("mouseup", () => {
+  isDraggingPopup2 = false;
+  popup2.style.cursor = "grab";
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (!isDraggingPopup2) return;
+
+  const x = e.clientX - offsetX2;
+  const y = e.clientY - offsetY2;
+
+  popup2.style.left = x + "px";
+  popup2.style.top = y + "px";
+});
